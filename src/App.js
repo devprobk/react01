f5e329c21c0da169cae79d1e6193727d75c1c25c
devprobk <devprobk@gmail.com>
2020-04-07 12:59:48 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt natus voluptates
+        libero odio tempore mollitia repellendus maxime, eum illum dolores voluptas quis unde quo
+        delectus, illo debitis aspernatur eos.
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
