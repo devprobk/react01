4f246067d29efb73206089775d5dfb268c20129d
devprobk <devprobk@gmail.com>
2020-04-07 22:02:11 +0200

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: white;
+  background-color: #61dafb;
+  border: none;
+  font-size: 18px;
+  text-align: center;
+`;
 
 const App = () => (
   <div className="App">
@@ -9,6 +19,7 @@ const App = () => (
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
+      <Button>This is my styled button</Button>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt natus voluptates
         libero odio tempore mollitia repellendus maxime, eum illum dolores voluptas quis unde quo
