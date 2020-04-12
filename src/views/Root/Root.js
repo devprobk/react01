6cd6ff17b9e7a353a1eb76a73b654b59d271ab06
devprobk <devprobk@gmail.com>
2020-04-12 14:41:13 +0200

    added env settings for news project added start project structure before styled components

---
+++ src/views/Root/Root.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import logo from './logo.svg';
+
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   color: white;
+//   background-color: #61dafb;
+//   border: none;
+//   font-size: 18px;
+//   text-align: center;
+// `;
+
+const Root = () => <h1>Hello Kamil</h1>;
+
+export default Root;
